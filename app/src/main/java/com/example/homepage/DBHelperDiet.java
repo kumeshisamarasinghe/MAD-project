50005305d1b34c92f81dc9944d977bad0929f1a8
Buddhini-123 <83524778+Buddhini-123@users.noreply.github.com>
2021-10-03 23:32:46 +0530

    Update DBHelperDiet.java

--- app/src/main/java/com/example/homepage/DBHelperDiet.java
+++ app/src/main/java/com/example/homepage/DBHelperDiet.java
@@ -14,22 +14,24 @@ public class DBHelperDiet extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase DB) {
-        DB.execSQL("create Table Dietdetails(category TEXT primary key, breakfast TEXT, lunch TEXT, dinner TEXT)");
+        //create the table
+        DB.execSQL("create Table Dietdetails(category TEXT primary key, weight TEXT, height TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        //check the created table already exist
         DB.execSQL("drop Table if exists Dietdetails");
     }
 
-    public Boolean insertdietdata(String category, String weight, String height, String state)
+    public Boolean insertdietdata(String category, String weight, String height)
     {
         SQLiteDatabase DB = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
+        //get values
         contentValues.put("category", category);
-        contentValues.put("breakfast", weight);
-        contentValues.put("lunch", height);
-        contentValues.put("dinner", state);
+        contentValues.put("weight", weight);
+        contentValues.put("height", height);
         long result=DB.insert("Dietdetails", null, contentValues);
         if(result==-1){
             return false;
@@ -39,12 +41,13 @@ public class DBHelperDiet extends SQLiteOpenHelper {
     }
 
 
-    public Boolean updatedietdata(String category, String weight, String height, String state) {
+    public Boolean updatedietdata(String category, String weight, String height) {
         SQLiteDatabase DB = this.getWritableDatabase();
+        //get values
         ContentValues contentValues = new ContentValues();
-        contentValues.put("breakfast", weight);
-        contentValues.put("lunch", height);
-        contentValues.put("dinner", state);
+        contentValues.put("weight", weight);
+        contentValues.put("height", height);
+        //do update according to the category
         Cursor cursor = DB.rawQuery("Select * from Dietdetails where category = ?", new String[]{category});
         if (cursor.getCount() > 0) {
             long result = DB.update("Dietdetails", contentValues, "category=?", new String[]{category});
@@ -61,6 +64,7 @@ public class DBHelperDiet extends SQLiteOpenHelper {
     public Boolean deletediet (String category)
     {
         SQLiteDatabase DB = this.getWritableDatabase();
+        //delete details according to the category
         Cursor cursor = DB.rawQuery("Select * from Dietdetails where category = ?", new String[]{category});
         if (cursor.getCount() > 0) {
             long result = DB.delete("Dietdetails", "category=?", new String[]{category});
@@ -78,6 +82,7 @@ public class DBHelperDiet extends SQLiteOpenHelper {
     public Cursor getdiet ()
     {
         SQLiteDatabase DB = this.getWritableDatabase();
+        //select all in the table
         Cursor cursor = DB.rawQuery("Select * from Dietdetails", null);
         return cursor;
 
